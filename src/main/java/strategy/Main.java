a0219aa30d2d5988e334ad7a7460461b47d0e1c5
Quoc PHAN <phquoc25@gmail.com>
2022-03-13 15:53:11 +0700

    strategy pattern final version

--- src/main/java/strategy/Main.java
+++ src/main/java/strategy/Main.java
@@ -4,7 +4,8 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        TranslateService translateService = new TranslateServiceImpl();
+        List<TranslateStrategy> translateStrategyList = List.of(new EnTranslatorImpl(), new FRTranslatorImpl());
+        TranslateService translateService = new TranslateServiceImpl(translateStrategyList);
         List<String> languages = List.of("en", "fr", "ja", "vn", "es");
         languages.forEach(s -> System.out.println(translateService.sayHello(s)));
     }
--- src/main/java/strategy/TranslateServiceImpl.java
+++ src/main/java/strategy/TranslateServiceImpl.java
@@ -1,18 +1,20 @@
 package strategy;
 
+import java.util.List;
+
 public class TranslateServiceImpl implements TranslateService {
+    private final List<TranslateStrategy> translateStrategyList;
+
+    public TranslateServiceImpl(List<TranslateStrategy> translateStrategyList) {
+        this.translateStrategyList = translateStrategyList;
+    }
 
     @Override
     public String sayHello(String language) {
-        switch (language) {
-            case "es":
-                return "Hola!";
-            case "ja":
-                return "Kon'nichiwa";
-            case "en":
-                return "Hello";
-            default:
-                return "";
-        }
+        return translateStrategyList.stream()
+                .filter(translateStrategy -> translateStrategy.support(language))
+                .findFirst()
+                .map(TranslateStrategy::sayHello)
+                .orElse("I don't know");
     }
 }
